50189984e8147834bf20e2d6b8142fd38fde17e3
Diego Torres <diegounamfi@gmail.com>
2017-02-05 10:54:46 -0600

    Added new PostVC

---
+++ MyHood/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  MyHood
+//
+//  Created by Diego Torres on 2/5/17.
+//  Copyright © 2017 UNAMunam. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(post: Post){ //takes in a type of class Post (immg, and 2 lbl)
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+    }
+
+}
--- MyHood/ViewController.swift
+++ MyHood/ViewController.swift
@@ -18,6 +18,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        let post1 = Post(imagePath: "", title: "postOne", description: "prueba de post")
+        let post2 = Post(imagePath: "", title: "postTwo", description: "prueba de post")
+        let post3 = Post(imagePath: "", title: "postThree", description: "prueba de post")
+        
+        posts.append(post1)
+        posts.append(post2)
+        posts.append(post3)
+        
+        tableView.reloadData()
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -29,7 +39,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        let post = posts[indexPath.row]
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell{
+            cell.configureCell(post: post)
+            return cell
+        }else{
+            let cell = PostCell()
+            cell.configureCell(post: post)
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
